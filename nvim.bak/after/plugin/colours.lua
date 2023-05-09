d4833796176b6e8021c3ee53118cbfddbc554766
kobejk <105041728+kobejk@users.noreply.github.com>
2023-05-09 14:29:14 +1000

    removed old configs

--- nvim.bak/after/plugin/colours.lua
+++
@@ -1,9 +0,0 @@
--- change the theme
--- rose pine is the default
-
-function ColourMyPencils(colour)
-    colour = colour or "rose-pine"
-    vim.cmd.colorscheme(colour)
-end
-
-ColourMyPencils()
--- nvim.bak/lua/kob/init.lua
+++
@@ -1,9 +0,0 @@
--- print("hello from kob")
--- use remaps
-require("kob.remap")
-
--- use nvim settings
-require("kob.set")
-
--- use packer
-require("kob.packer")
